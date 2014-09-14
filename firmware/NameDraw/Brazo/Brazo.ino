36cae9195051d88e16ad6cb25c9e2c818e1a91ec
algspd <algspd@portatil>
2014-09-14 20:27:57 +0200

    Some characters written ok

--- firmware/NameDraw/Brazo/Brazo.ino
+++ firmware/NameDraw/Brazo/Brazo.ino
@@ -1,12 +1,14 @@
 #include <Servo.h>
 #include "Letras.h"
 
+#define DEBUG false
+
 double const l1=74; // First arm length (mm)
-double const l2=129; // Second arm length (mm)
+double const l2=130; // Second arm length (mm)
 double x=0,y=0,oldx,oldy;
 double theta1,theta2;
 double bl;
-double const minstep=1.0; //mm
+double const minstep=2; //mm
 double maxx=36;
 double const kerning=1;
 
@@ -36,13 +38,12 @@ void setup() {
   boli.attach(11);
   boli.writeMicroseconds(1900);
   // Wait for the pen to be up
-  delay(300);
+  delay(200);
   servo1.attach(9);
   servo2.attach(10);
   
   // Wait for the arm to be in start position
-  delay(1000);
-  //boli.writeMicroseconds(1400);
+  delay(100);
   
   // Say hello
   Serial.println("Online :)");
@@ -160,6 +161,8 @@ void loop() {
 }
 
 void moveTo(double x, double y){
+    if (x==0) x=0.1;
+    if (y==0) y=0.1;
     maxx=max(x,maxx);
     bl=sqrt(x*x+y*y); // (x,y) vector length
     
@@ -173,11 +176,8 @@ void moveTo(double x, double y){
       
       // Angle between first arm and the vector
       theta1=PI/2-atan2(y,x)-asin((l2*sin(o2))/bl);
-      //theta1=acos((bl*bl+l1*l1-l2*l2)/(2*bl*l1))*180/PI;
-      //theta1+=atan(y/x);
       
       // Angle between the first and the second arm 
-      //theta2=acos((bl*bl+l2*l2-l1*l1)/(2*bl*l2))*180/PI;
       theta2=PI-o2;
       
       // Convert both angles to degrees
@@ -188,13 +188,13 @@ void moveTo(double x, double y){
         Serial.println("------------> NO HAY SOLUCION EN EL RECORRIDO DE LOS SERVOS");
       }
       else {
-        // *1600/180+700 Hace la conversion del rango 0-180 a 700-2300
+        // *1600/180+700 converts range 0-180 to 700-2300
         servo1.writeMicroseconds((theta1+90)*1600/180+700);
         servo2.writeMicroseconds((theta2+90)*1600/180+700);
       }
 
       // FIXME: Calcular el tiempo de desplazamiento dinamicamente
-      delay(50);
+      delay(20);
       
       Serial.print(" Theta: ");Serial.print(theta1,2);Serial.print(",");Serial.println(theta2,2);
     } else {
@@ -206,11 +206,8 @@ void moveTo(double x, double y){
 // al indicado como parametro 
 void writeline(double x,double y, double v){
   double newx,newy;
-  //Calculate number of gegments
-  int segments=sqrt((x-oldx)*(x-oldx)+(y-oldy)*(y-oldy))/minstep;
-  //Fix number of segments for debug
-  //segments=10;
-  
+  int segments;
+
   Serial.print("Moving to (");
   Serial.print(x);
   Serial.print(",");
@@ -218,10 +215,12 @@ void writeline(double x,double y, double v){
   Serial.print(") in ");
   
   // Interpolate
-  if (v==1) {
+  if (v==1) {    
+    //Calculate number of segments
+    segments=sqrt((x-oldx)*(x-oldx)+(y-oldy)*(y-oldy))/minstep;
     delay(100);
     boli.writeMicroseconds(1400);
-    
+    delay(100);
     Serial.print(segments);
     Serial.println(" segments.");
     for (i=1;i<=segments-1;i++){
@@ -230,71 +229,64 @@ void writeline(double x,double y, double v){
       moveTo(newx,newy);
     }
   }
-  else {
-    // if v==0
+  else { // if v==0
+    // 1 Quick segment
+    delay(100);
     boli.writeMicroseconds(1800);
     delay(100);
-    Serial.println("1 quick segemnt.");
+    moveTo(x,y);
+    delay(100);
   }
   
   //To be sure of final position
   moveTo(x,y);
 
-  // Send angles over serial, for debugging purposes
-  Serial.print("Theta1: ");Serial.println(theta1,2);
-  Serial.print("Theta2: ");Serial.println(theta2,2);
+  if (DEBUG){
+    // Send angles over serial, for debugging purposes
+    Serial.print("Theta1: ");Serial.println(theta1,2);
+    Serial.print("Theta2: ");Serial.println(theta2,2);
+  }
   
   // FIXME: actualizar solo si el brazo realmente se ha movido
   oldx=x;
   oldy=y;
-  
 }
 
 void printletter(char l) {
   int i;
-  double x, y, v, xoffset;
+  double x, y, v, xoffset, antx, anty ,antv;
   xoffset=maxx+kerning;
   
-  Serial.write(l);
-  Serial.println();
-  if ( l <= 'N' ) { //Mayusculas antes de la Ñ
+  Serial.println(l);
+  if ( l <= 'N' ) { // Uppercase before Ñ
     i=l-'A';
-    Serial.println("Mayusculas antes de la ENHE");
-  } else if (l == 'Ñ') {//La Ñ
+  } else if (l == 'Ñ') {// Ñ
     i='N'-'A'+1;
-    Serial.println("La ENHE");
-  } else if (l <= 'Z') {//Mayusculas despues de la Ñ
+  } else if (l <= 'Z') {// Uppercase after Ñ
     i=l-'A'+1;
-    Serial.println("Mayusculas despues de la ENHE");
-  } else if (l <= 'n') {//Minusculas antes de la ñ
+  } else if (l <= 'n') {// Lowercase before ñ
     i=l-'a'+'Z'-'A'+2;
-    Serial.println("Minusculas antes de la enhe");    
-  } else if (l == 'ñ' ) {//La ñ
+  } else if (l == 'ñ' ) {// ñ
     i='n'-'a'+1+'Z'-'A'+2;
-    Serial.println("La enhe");    
-  } else if (l <= 'z') {//Minusculas despues de la ñ
-    i=l-'a'+'Z'-'A'+3;
-    Serial.println("Minusculas despues de la enhe");    
+  } else if (l <= 'z') {// Lowercase after ñ
+    i=l-'a'+'Z'-'A'+3;  
   }
-  
-  //Serial.println(i);
-  Serial.println();
+  xoffset=50;
 
-    x = pgm_read_float( &alfabeto[i][0][0] );
-    y = pgm_read_float( &alfabeto[i][0][1] );
-    writeline(x+xoffset, y+146, 0);
     
-    for (j=0; j<25; j++) {
-        //Read x
-        x = pgm_read_float( &alfabeto[i][j][0] );
-        //Read y
-        y = pgm_read_float( &alfabeto[i][j][1] );
-        //Read v
-        v = pgm_read_float( &alfabeto[i][j][2] );
-        //v=0;
-      xoffset=0;
-      writeline(x+xoffset, y+146, v);
-    }
-    Serial.println();
+  for (j=0; j<25; j++) {
+    //Read x
+    x = pgm_read_float( &alfabeto[i][j][0] );
+    //Read y
+    y = pgm_read_float( &alfabeto[i][j][1] );
+    //Read v
+    v = pgm_read_float( &alfabeto[i][j][2] );
+     
+    if (antx==x && anty==y && antv==v) break;
+    antx=x;anty=y;antv=v;
+      
+    writeline(x+xoffset, y+146, v);
+  }
+  Serial.println();
 }
 
